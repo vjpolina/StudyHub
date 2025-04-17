b6c0fce02ebbc806a07f7dbf81c8184d0bedcab7
vjpolina <voljinpolina@gmail.com>
2025-04-17 13:33:46 +0300

    Update notes1.js
    - Fixed issues with saving format

--- notes1.js
+++ notes1.js
@@ -1,37 +1,54 @@
 const notesContainer = document.querySelector('.notes-container');
 const addButton = document.querySelector('.add');
-let notes = document.querySelectorAll('.input-box');
+
 
 function loadNotes() {
-    notesContainer.innerHTML = localStorage.getItem('notes');
+    const savedNotes = JSON.parse(localStorage.getItem('notes')) || [];
+    notesContainer.innerHTML = '';
+    savedNotes.forEach(noteContent => {
+        createNoteElement(noteContent);
+    });
 }
-loadNotes();
+
 
 function saveNotes() {
-    localStorage.setItem('notes', notesContainer.innerHTML);
+    const notes = Array.from(notesContainer.children).map(note => {
+        const noteContent = note.querySelector('.input-box');
+        return noteContent ? noteContent.innerHTML : '';
+    });
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
+
+function createNoteElement(content = '') {
+    let newNote = document.createElement('div');
+    let noteContent = document.createElement('p');
+    let newImg = document.createElement('img');
+
+    newNote.className = 'note-wrapper';
+    noteContent.className = 'input-box';
+    newImg.className = 'deleteBtn';
+
+    noteContent.setAttribute('contenteditable', 'true');
+    newImg.src = 'C:/Users/PC/Desktop/practica/StudyHub/img/delete_final.png';
+
+    noteContent.innerHTML = content;
+    newNote.appendChild(noteContent);
+    newNote.appendChild(newImg);
+    notesContainer.appendChild(newNote);
+
+    noteContent.addEventListener('keyup', () => {
+        saveNotes();
+    });
+
+    newImg.addEventListener('click', () => {
+        newNote.remove();
+        saveNotes();
+    });
 }
 
 addButton.addEventListener('click', () => {
-    let newNote = document.createElement('p');
-    let newImg = document.createElement('img');
-    newNote.className = 'input-box';
-    newImg.className = 'deleteBtn';
-    newNote.setAttribute('contenteditable', 'true');
-    newImg.src = 'C:/Users/PC/Desktop/practica/StudyHub/img/delete.png';
-    notesContainer.appendChild(newNote);
-    newNote.appendChild(newImg);
+    createNoteElement();
+    saveNotes();
 });
 
-notesContainer.addEventListener('click', function(e){
-    if(e.target.tagName === 'IMG'){
-        e.target.parentElement.remove();
-        saveNotes();
-    }
-    else if(e.target.tagName === 'P'){
-        notes = document.querySelectorAll('.input-box');
-        notes.forEach(nt => {
-            nt.onkeyup = function() {
-                saveNotes();
-            };
-    });
-}});
+loadNotes();
